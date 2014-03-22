77a0c87315841f13cf2c877558f99c6f26207f71
Rich Dougherty <rich@rd.gen.nz>
2014-03-22 18:06:32 +1300

    Restructured into CommonJS modules

---
+++ Gruntfile.js
@@ -0,0 +1,91 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    artifactName: 'interpreter',
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/<%= artifactName %>.js'],
+        dest: 'dist/<%= artifactName %>.js'
+      },
+    },
+    browserify: {
+      dist: {
+        src: ['lib/<%= artifactName %>.js'],
+        dest: 'dist/<%= artifactName %>.js',
+        bundleOptions: {
+          standalone: 'Interpreter'
+        }
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= artifactName %>.min.js'
+      },
+    },
+    nodeunit: {
+      files: ['test/**/*_test.js']
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        options: {
+          jshintrc: 'lib/.jshintrc'
+        },
+        src: []
+      },
+      test: {
+        src: ['test/**/*.js']
+      },
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'nodeunit']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'nodeunit']
+      },
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'nodeunit', 'browserify', 'uglify']);
+
+};
---
+++ test/interpreter_test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var Interpreter = require('../lib/interpreter.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['awesome'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'run integer expression': function(test) {
+    var reflGlobal;
+    var interp = new Interpreter('var x = 1;', function(interp, scope) {
+      reflGlobal = scope;
+    });
+    interp.run();
+    var reflx = interp.getProperty(reflGlobal, "x");
+    var refl1 = interp.createPrimitive(1);
+    var comp = interp.comp(reflx, refl1);
+    test.equal(comp, 0);
+    test.done();
+  }
+};
